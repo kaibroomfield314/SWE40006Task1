0b1ff3c2e77236187145bfdb7b8a3db925fd5387
kaibr <kaibr@KAIWINDOWSVM>
2026-08-20 12:29:29 +1000

    Add project files.

---
+++ CountApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CounterLogic;
+
+namespace CountApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Count App!");
+            Console.WriteLine("Press any key to start counting up to 10");
+            Console.ReadKey(true);
+            int count = 0;
+            while (count < 10)
+            {
+                count++;
+                Console.WriteLine(Counter.CountFormater(count));
+                Console.ReadKey(true);
+            }
+            Console.WriteLine("Done!");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ CounterLogic/Counter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterLogic
+{
+    public class Counter
+    {
+        public static string CountFormater(int count) 
+        {
+            return $"Count: {count}";
+        }
+    }
+}
